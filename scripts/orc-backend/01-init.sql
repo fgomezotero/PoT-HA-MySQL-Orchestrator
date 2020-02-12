c864408412b4f6cb816ee05a7c5811deeb8bc8a5
Franklin Gómez <fgomezotero@gmail.com>
2020-02-12 14:52:31 -0300

    Orchestrator with SQLite as a backups database

--- scripts/orc-backend/01-init.sql
+++
@@ -1,6 +0,0 @@
-######################################################################
-# MySQL backend DB setup
-#######################################################################
-
-CREATE USER 'orc_server_user'@'%' IDENTIFIED BY 'orc_server_password';
-GRANT ALL ON orchestrator.* TO 'orc_server_user'@'%';
